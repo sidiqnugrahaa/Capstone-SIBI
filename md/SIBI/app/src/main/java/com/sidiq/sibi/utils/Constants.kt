cd6e7e3e842a09cc1704afb1c9b1de51cdef5f42
priyayidimas <priyayidimas@upi.edu>
2021-05-31 15:41:40 +0700

    Update Constants

--- md/SIBI/app/src/main/java/com/sidiq/sibi/utils/Constants.kt
+++ md/SIBI/app/src/main/java/com/sidiq/sibi/utils/Constants.kt
@@ -1,3 +1,9 @@
 package com.sidiq.sibi.utils
 
 const val COLLECTION_USER = "users"
+const val COLLECTION_HISTORY = "history"
+const val FIELD_SCORE = "score"
+const val LIMIT_RANK = 10L
+const val FIELD_TIMESTAMP = "timestamp"
+
+const val BASE_URL = ""
