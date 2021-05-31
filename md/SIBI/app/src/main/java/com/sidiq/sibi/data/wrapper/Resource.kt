553a6e042aa693b28b5a7a71efaebe1c379b3d9e
priyayidimas <priyayidimas@upi.edu>
2021-05-31 15:08:25 +0700

    Add Empty Wrapper

--- md/SIBI/app/src/main/java/com/sidiq/sibi/data/wrapper/Resource.kt
+++ md/SIBI/app/src/main/java/com/sidiq/sibi/data/wrapper/Resource.kt
@@ -3,5 +3,6 @@ package com.sidiq.sibi.data.wrapper
 sealed class Resource<out T> {
     class Loading<out T> : Resource<T>()
     data class Success<out T>(val data: T) : Resource<T>()
+    class Empty<out T> : Resource<T>()
     data class Failure<out T>(val throwable: Throwable) : Resource<T>()
 }
